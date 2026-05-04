c7e42fba521cc2527e7b6d0f7fc0a74d289feed7
Tetsu42 <168170963+Tetsu42@users.noreply.github.com>
2026-05-04 11:32:58 +0000

    Ajout d'event de deplacement

--- game/scenes/GameScene.js
+++ game/scenes/GameScene.js
@@ -16,9 +16,32 @@ export default class GameScene extends Scene {
         this.lia.sprite.setPosition(400, 400)
 
         this.physics.add.collider(this.lia.sprite, manager.getWallGroup(this, 'default_map'))
+
+        this.keys = this.input.keyboard.addKeys({
+            up: Phaser.Input.Keyboard.KeyCodes.W,
+            down: Phaser.Input.Keyboard.KeyCodes.S,
+            left: Phaser.Input.Keyboard.KeyCodes.A,
+            right: Phaser.Input.Keyboard.KeyCodes.D
+        })
+
     }
 
     update() {
+
+        //mouvement via WASD 
+        if (this.keys.left.isDown) {
+            this.lia.sprite.setVelocityX(-this.lia.speed)
+        }else if (this.keys.right.isDown) {
+            this.lia.sprite.setVelocityX(this.lia.speed)
+        } else if (this.keys.up.isDown) {
+            this.lia.sprite.setVelocityY(-this.lia.speed)
+        } else if (this.keys.down.isDown) {
+            this.lia.sprite.setVelocityY(this.lia.speed)
+        } else {
+            this.lia.sprite.setVelocity(0)
+        }
+
+        //mouvement via flèches directionnelles
         this.maki.move(this.lia)
     }
 }
